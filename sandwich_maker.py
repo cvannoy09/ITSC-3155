ed05d477424b529b52525a45b70bb8eb90499627
cvannoy09 <calebvannoy4@gmail.com>
2024-09-25 21:52:49 -0400

    Update sandwich_maker.py

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -1,12 +1,23 @@
-d
+
 class SandwichMaker:
     def __init__(self, resources):
         self.machine_resources = resources
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+         for ingredient, amount_needed in ingredients.items():
+            if self.machine_resources.get(ingredient, 0) < amount_needed:
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########   
-dfdfdfd
+      sandwich_recipe = self.recipes[sandwich_size]
+        ingredients = sandwich_recipe["ingredients"]
+
+        if self.check_resources(ingredients):
+          
+            for ingredient, amount_needed in ingredients.items():
+                self.machine_resources[ingredient] -= amount_needed
+            return True  
+        else:
+            return False 
